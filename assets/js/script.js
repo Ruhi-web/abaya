434720161f88b6f306c26e26e1c9005e7efc4855
ruhi <ruhinaz@gmail.com>
2021-11-08 22:31:03 +0400

    pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,28 @@
        })
      }
      $(window).on('load resize scroll', checkScrollBar)
-     });
-
-
-   
+     
+    });
+// carousal
+$(document).ready(function(){
+     $("#carousel-1").carousel({
+        interval: 3000
+      });
+      
+      $("#carousel-1.carousel .carousel-item").each(function() {
+        const total = 3;
+        let next = $(this).next();
+      
+        for (var i = 0; i < total - 1; i++) {
+          if (!next.length) {
+            next = $(this).siblings(":first");
+          }
+          next
+            .children(":first-child")
+            .clone()
+            .appendTo($(this));
+          next = next.next();
+        }
+      });
+      
+});

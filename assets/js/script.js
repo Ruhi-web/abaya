9c68787df2b749e907d7f89c8be107e3737d0a8c
ruhi <ruhinaz@gmail.com>
2021-11-10 00:09:03 +0400

    edit1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,38 @@
-      
-      // navbar color change onscroll
-      $(document).ready(function(){
-
-     var checkScrollBar = function(){
-       $('nav').css({
-         backgroundColor: $(this).scrollTop() > 1 ?
-           'rgb(0, 0, 0)' : 'rgba(0, 0, 0, 0.164)'
-
-       })
-     }
-     $(window).on('load resize scroll', checkScrollBar)
-     
-    });
+// navbar color change onscroll
+$(document).ready(function () {
+	var checkScrollBar = function () {
+		$("nav").css({
+			backgroundColor: $(this).scrollTop() > 1 ? "rgb(0, 0, 0)" : "rgba(0, 0, 0, 0.164)",
+		});
+	};
+	$(window).on("load resize scroll", checkScrollBar);
+});
 // carousal
-$(document).ready(function(){
-     $("#carousel-1").carousel({
-        interval: 3000
-      });
-      
-      $("#carousel-1.carousel .carousel-item").each(function() {
-        const total = 3;
-        let next = $(this).next();
-      
-        for (var i = 0; i < total - 1; i++) {
-          if (!next.length) {
-            next = $(this).siblings(":first");
-          }
-          next
-            .children(":first-child")
-            .clone()
-            .appendTo($(this));
-          next = next.next();
-        }
-      });
-      
+$(document).ready(function () {
+	$("#carousel-1").carousel({
+		interval: 3000,
+	});
+
+	$("#carousel-1.carousel .carousel-item").each(function () {
+
+		var width = window.innerWidth;
+		var height = window.innerHeight;
+
+		console.log(width);
+		if (width < 1080) {
+            var total = 1;
+		} else {
+            var total = 3;
+		}
+
+		let next = $(this).next();
+
+		for (var i = 0; i < total - 1; i++) {
+			if (!next.length) {
+				next = $(this).siblings(":first");
+			}
+			next.children(":first-child").clone().appendTo($(this));
+			next = next.next();
+		}
+	});
 });
